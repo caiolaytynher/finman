60c11efa86770458f0354d89bf78944b88230931
caiolaytynher <csrlaytynher@proton.me>
2024-04-23 19:59:58 -0300

    new in progress main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Finman in progress...
+    <main className="flex min-h-screen items-center justify-center">
+      <div className="text-4xl">Finman in progess...</div>
     </main>
   );
 }
