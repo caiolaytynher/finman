6561908d0ffe3ffdec1ae0b93dd04303c3d56ebb
caiolaytynher <csrlaytynher@proton.me>
2024-04-23 23:34:33 -0300

    added mock posts to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Link from "next/link";
+import { db } from "~/server/db";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <div className="text-4xl">Finman in progess...</div>
+      <div className="flex flex-col">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+      </div>
     </main>
   );
 }
